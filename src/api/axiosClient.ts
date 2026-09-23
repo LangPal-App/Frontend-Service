1d9168d8a6373436c0cbd05e0525dd2f4dd12c6f
Ahmed_Alkholy <ahmedalkholy@protonmail.com>
2026-09-24 01:15:08 +0300

    feat: change api request 'Accept-Language' header when language is changed

--- src/api/axiosClient.ts
+++ src/api/axiosClient.ts
@@ -1,8 +1,17 @@
 import axios from 'axios';
 import { API_BASE_URL } from './config';
 
+import i18n from '../i18n';
+
 export const axiosClient = axios.create({
   baseURL: API_BASE_URL,
   withCredentials: true,
-  headers: { Accept: 'application/json' },
+  headers: {
+    Accept: 'application/json',
+    'Accept-Language': i18n.language,
+  },
+});
+
+i18n.on('languageChanged', (lng: string) => {
+  axiosClient.defaults.headers['Accept-Language'] = lng;
 });
